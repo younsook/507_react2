bd2a88858320d835b3bac1dc901ff9be24ff7748
younsook <hodu0622@naver.com>
2025-07-21 16:33:15 +0900

    Initial commit

---
+++ postcss.config.cjs
@@ -0,0 +1,8 @@
+const tailwind = require('@tailwindcss/postcss');
+const autoprefixer = require('autoprefixer');
+module.exports = {
+    plugins: [
+        tailwind(),
+        autoprefixer(),
+    ],
+};
